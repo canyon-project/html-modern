7496f87c7724cc9a6183dff859dcb8320b903abe
Travis Zhang <wr.zhang25@gmail.com>
2026-09-14 16:03:02 +0800

    feat: update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vitest/config";
+const htmlModernReporter = join(process.cwd(), "dist/reporter.js");
+import { join } from "node:path";
 
 import pkg from "./package.json" with { type: "json" };
 
 export default defineConfig({
   test: {
     name: { label: pkg.name, color: "cyan" },
+    coverage: {
+      enabled: true,
+      reportsDirectory: "./coverage/raw",
+      reporter: [htmlModernReporter],
+    },
   },
 });
